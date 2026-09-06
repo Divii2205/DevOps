47e65ac471682a0f402b73e148bf3753ff3a5583
Divii2205 <divijaa22am@gmail.com>
2026-09-06 18:02:05 +0530

    session3-shell-scripting: add shell scripting task, script and output

---
+++ session3-shell-scripting/task-data/task.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Print current date
+date
+
+# Print hostname
+hostname
+
+# Print username
+whoami
+
+# Print disk usage
+df -h
+
+# Print running processes
+ps aux
+
+# Store data using variables
+username=$(whoami)
+current_date=$(date)
+
+echo "Username: $username"
+echo "Date: $current_date"
+
+# Take user input
+read -p "Enter directory name: " directory_name
+
+# Create a directory
+mkdir "$directory_name"
+
+# Create a file
+touch "$directory_name/processes.txt"
+
+# Store running processes in the file
+ps aux > "$directory_name/processes.txt"
+
+echo "Process information saved in $directory_name/processes.txt"
